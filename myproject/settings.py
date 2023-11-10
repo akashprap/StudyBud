f8c4439a4fd00b73b6a20f35828923433fde1185
akashprap <akashprap2002@gmail.com>
2023-11-10 14:56:19 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG","False").lower()=='true'
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
